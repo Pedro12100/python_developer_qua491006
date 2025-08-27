8235dc1140e89fd42ce52a6c5d896a63509edcb2
Pedro12100 <phbastos90@gmail.com>
2025-08-26 21:43:23 -0300

    aula 26/08

---
+++ BANCO_CONEXAO/01_crud_uma_entidade/database/-- SQLite.sql
@@ -0,0 +1,5 @@
+-- SQLite
+
+INSERT INTO pessoa (nome, email, data_nascimento) VALUES ("fulano", "fulano@gmail.com", 1989-02-06);
+
+INSERT INTO pessoa (nome, email, data_nascimento) VALUES ("Rafael", "rafaefafae@gmail.com", 2010-08-03);
