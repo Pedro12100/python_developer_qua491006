7945dacd3968285bc121652f557c68d027fcdcc8
Pedro12100 <phbastos90@gmail.com>
2025-08-28 21:41:42 -0300

    Aula 28/08

---
+++ BANCO_CONEXAO/01_crud_uma_entidade/entidade.py
@@ -0,0 +1,19 @@
+# importa a biblioteca sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.orm import declarative_base  # IMPORTANTE: faltava isso
+
+def criar_tb_pessoa(engine, Base):
+    try:
+        class Pessoa(Base):
+            __tablename__ = "pessoa"
+
+            id_pessoa = Column(Integer, primary_key=True, autoincrement=True)
+            nome = Column(String, nullable=False)
+            email = Column(String, nullable=False, unique=True)
+            data_nascimento = Column(Date, nullable=False)
+
+        Base.metadata.create_all(engine)
+
+        return Pessoa
+    except Exception as e:
+        print(f"Não foi possível conectar ao banco. {e}")
--- BANCO_CONEXAO/01_crud_uma_entidade/main.py
+++ BANCO_CONEXAO/01_crud_uma_entidade/main.py
@@ -1,91 +1,57 @@
-# importa a biblioteca sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
-import os 
+import arquivo as ar
 
-limpar = lambda: os.system("cls")
+Base = declarative_base()
 
-def criar_tb_pessoa(engine, Base):
+class Pessoa(Base):
+    __tablename__ = "pessoa"
+    id_pessoa = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    data_nascimento = Column(Date, nullable=False)
+
+def criar_tb_pessoa(engine):
     try:
-        class Pessoa(Base):
-            __tablename__ = "pessoa"
-
-            id_pessoa = Column(Integer, primary_key=True, autoincrement=True)
-            nome = Column(String, nullable=False)
-            email = Column(String, nullable=False, unique=True)
-            data_nascimento = Column(Date, nullable=False)
-
         Base.metadata.create_all(engine)
-
-        return Pessoa
     except Exception as e:
         print(f"Não foi possível conectar ao banco. {e}")
 
-def cadastrar_pessoa(session, Pessoa):
-    nome = input("Digite o nome do usuário: ").strip().title()
-    email = input("Digite o e-mail do usuário: ").strip().lower()
-    data_nascimento = input("Digite a data de nascimento (dd/mm/aaaa): ").strip()
-    data_nascimento = datetime.strptime(data_nascimento, "%d/%m/%Y").date()
-
-    nova_pessoa = Pessoa(nome=nome, email=email, data_nascimento=data_nascimento)
-
-    session.add(nova_pessoa)
-    session.commit()
-
-    print(f"Pessoa {nome} cadastro com sucesso.")
-
-def listar_pessoas(session, Pessoa):
-    pessoas = session.query(Pessoa).all()
-
-    print("\nPessoas cadastradas:\n")
-    for pessoa in pessoas:
-        print(f"ID: {pessoa.id_pessoa}")
-        print(f"Nome: {pessoa.nome}")
-        print(f"E-mail: {pessoa.email}")
-        print(f"Data de nascimento: {pessoa.data_nascimento.strftime("%d/%m/%Y")}")
-        print(f"{"-"*30}")
-
 def main():
     engine = create_engine("sqlite:///01_crud_uma_entidade/database/db_crud.db")
-    Base = declarative_base()
+    criar_tb_pessoa(engine)
 
-    Pessoa = criar_tb_pessoa(engine, Base)
-    
     Session = sessionmaker(bind=engine)
     session = Session()
 
     while True:
-        print(f"{"-"*20} CRUD {"-"*20} ")
+        print(f"{'-'*20} CRUD {'-'*20}")
         print("1 - Cadastrar nova pessoa")
-        print("2 - Listar pessoa")
+        print("2 - Listar pessoas")
         print("3 - Alterar dados de uma pessoa")
         print("4 - Excluir uma pessoa")
         print("5 - Sair do programa")
         opcao = input("Informe a opção desejada: ").strip()
-        limpar()
+        ar.limpar()
+
         match opcao:
             case "1":
-                cadastrar_pessoa(session, Pessoa)
-                continue
+                ar.cadastrar_pessoa(session, Pessoa)
             case "2":
-                listar_pessoas(session, Pessoa)
-                continue
+                ar.listar_pessoas(session, Pessoa)
             case "3":
-                pass
+                ar.pesquisar_pessoa(session, Pessoa)
             case "4":
-                pass
+                print("Funcionalidade ainda não implementada.")
             case "5":
                 print("Programa encerrado.")
+                break
             case _:
-                print("Opção invalida!")
+                print("Opção inválida.")
                 continue
-        cadastrar_pessoa(session, Pessoa)
 
     session.close()
 
-
-if __name__ == "__main__":  # Corrigido aqui
+if __name__ == "__main__":
     main()
-
-
