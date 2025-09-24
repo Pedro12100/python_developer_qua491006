c8be223da8ee209d77964753d1dfbe9775046d1b
Pedro12100 <phbastos90@gmail.com>
2025-09-23 21:35:59 -0300

    Aula 23/09

--- 10_django/projeto_crud/crud_app/views.py
+++ 10_django/projeto_crud/crud_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Pessoa
 
-<<<<<<< HEAD
 # Create your views here.
 def home(request):
     pessoas = Pessoa.objects.all()
@@ -30,27 +29,8 @@ def deletar_pessoa(request, id_pessoa):
     pessoa = Pessoa.objects.get(id_pessoa=id_pessoa)
     pessoa.delete()
     return redirect('home')
-=======
-# Página inicial: lista todas as pessoas
-def home(request):
-    pessoas = Pessoa.objects.all()
-    return render(request, "home.html", {"pessoas": pessoas})
 
-
-# Página de cadastro de pessoa
-def cadastro_pessoa(request):
-    if request.method == "POST":
-        nome = request.POST.get("nome")
-        email = request.POST.get("email")
-        cpf = request.POST.get("cpf")
-
-        Pessoa.objects.create(
-            nome=nome,
-            email=email,
-            cpf=cpf
-        )
-
-        return redirect("home")  # volta pra home depois de cadastrar
-
-    return render(request, "cadastrar.html")
->>>>>>> a4e35f3e5af471a8141c901b06f799f79ccedde5
+def buscar_pessoa(request):
+    query = request.GET.get('buscar', '')
+    pessoas = Pessoa.objects.filter(nome__icontains=query)
+    return render(request, 'home.html', {'pessoas': pessoas, 'query': query})
